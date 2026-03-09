117a517c07af055493b1ec3f2ba8c07b6468f529
mathieu <mathieu.beriac@ynov.com>
2026-03-09 10:04:32 +0100

    feat: :construction: Added first version of incrementation api

---
+++ csgo/lib/services/csgo_api_service.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class CsgoApiService {
+  static const String _baseUrl = 'https://raw.githubusercontent.com/ByMykel/CSGO-API/main/public/api';
+
+  final String language;
+
+  CsgoApiService({this.language = 'fr'});
+
+  Future<List<dynamic>> getSkins() async {
+    final reponse = await http.get(
+      Uri.parse('$_baseUrl/$language/skins.json'),
+    );
+
+    if (reponse.statusCode ==200) {
+      return jsonDecode(reponse.body);
+    } else {
+      throw Exception('Failed to load skins');
+    }
+  }
+
+  Future <List<dynamic>> getCases() async {
+    final reponse = await http.get(
+      Uri.parse('$_baseUrl/$language/crates.json'),
+    );
+
+    if (reponse.statusCode ==200) {
+      return jsonDecode(reponse.body);
+    } else {
+      throw Exception('Failed to load cases');
+    }
+}
+
+  Future <List<dynamic>> getHighlight() async {
+    final reponse = await http.get(
+      Uri.parse('$_baseUrl/$language/highlights.json'),
+    );
+
+    if (reponse.statusCode ==200) {
+      return jsonDecode(reponse.body);
+    } else {
+      throw Exception('Failed to load highlight');
+    }
+}
+}
